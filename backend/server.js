2cfc372f9d3bfed07fab62d01ad37f3d37f3efdb
rajulapatibalaji931-cloud <rajulapatibalaji931@gmail.com>
2026-06-28 18:40:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${5000}`);
 });
